9f3c9fdfd6857723ca799d66d347f10b69f3531d
kabirbenepass <63077963+kabirbenepass@users.noreply.github.com>
2021-09-20 00:23:18 -0400

    woo

--- src/index.js
+++ src/index.js
@@ -1,90 +1,115 @@
 import jp from "jsonpath";
 import { data } from "./test";
+import _ from "lodash";
 
-let parser = {};
-
-const parent = (o) => Object.getPrototypeOf(o).name;
-
-class Transformer {
+class Transform {
   constructor() {
     this.index = {};
-    this.tree = {};
+    this.roots = [];
+    this.register = this._register.bind(this);
+    this.wildcard = {};
   }
 
-  when(cond, cls) {
-    const A = cls.name;
-    const B = parent(cls);
-    console.log("args", A, B);
-    let entry = {
-      name: A,
-      cls,
-      conds: [cond],
-      children: []
+  _register({ elements, kind }) {
+    const base = Object.getPrototypeOf;
+    return {
+      kind,
+      elements,
+      finisher: (cls) => {
+        const name = cls.name;
+
+        if (name in this.index) {
+          throw new Error(`Cannot re-register class ${name}`);
+        }
+        if (!cls.spec || cls.spec == base(cls).spec) {
+          cls.spec = this.wildcard;
+        }
+        this.index[name] = {
+          cls: cls,
+          name: name,
+          cond: cls.spec,
+          children: []
+        };
+
+        let baseClass = base(cls);
+        let baseName = baseClass.name;
+        while (baseName) {
+          if (baseName in this.index) {
+            this.index[name].baseName = baseName;
+            this.index[baseName].children.push(this.index[name]);
+            return cls;
+          } else {
+            baseClass = base(baseClass);
+            baseName = baseClass.name;
+          }
+        }
+        this.roots.push(this.index[name]);
+        return cls;
+      }
     };
-    if (A in this.index) {
-      this.index[A].conds.push(cond);
-      return this;
-    }
-    this.index[A] = entry;
-    if (B in this.index) {
-      this.index[B].children.push(entry);
-    } else {
-      this.tree[A] = entry;
-    }
-    return this;
   }
 
-  run(object, ctx = {}, current = null) {
-    let options = current
-      ? current.children
-      : [...Object.keys(this.tree)].map((k) => this.index[k]);
-
+  run(object, context = {}, options = null) {
+    options = options || this.roots;
     for (let opt of options) {
-      for (let cond of opt.conds) {
-        if (cond(object, ctx)) {
-          return this.run(object, ctx, opt) || new opt.cls(object, this, ctx);
+      if (opt.cond == this.wildcard || opt.cond(object)) {
+        const specialized = this.run(object, context, opt.children);
+        if (specialized) {
+          return specialized;
+        } else if (opt.cond != this.wildcard) {
+          return new opt.cls(object, context);
         }
       }
     }
-
-    if (current === null) {
-      throw new Error(`Unmatchable object: ${object.name} ${object.type}`);
-    }
-    return false;
   }
 }
 
+const transform = new Transform();
+
+@transform.register
 class Component {
-  constructor(args, xfrm, ctx) {
-    this.name = args.name || args.type;
+  constructor(object, context) {
+    this.name = object.name;
+    this.type = object.type;
+  }
+
+  render() {
+    return {};
   }
 }
 
-class Button {
-  constructor(args, xfrm, ctx) {
-    this.id = `id${Math.random()}`;
-    this.type = "Button";
-    this.props = {
-      title: args.children[0].children[0].name
-    };
+@transform.register
+class Frame extends Component {
+  static spec = (o) =>
+    o.type in { FRAME: 1, CANVAS: 1 } && !o.name.startsWith("Button");
+  constructor(object, context) {
+    super(object, context);
+    this.addChildren(object.children, context);
+  }
+
+  addChildren(children, context) {
+    this.children = _(children)
+      .map((c) => transform.run(c, context))
+      .filter();
+  }
+
+  render() {
+    return {};
   }
 }
 
-class Container {
-  constructor(args, xfrm, ctx) {
-    this.name = args.name || args.type;
-    this.children = args.children.map((c) =>
-      xfrm.run(c, { ...ctx, parent: this })
-    );
+@transform.register
+class Button extends Component {
+  static spec = (o) => o.name.startsWith("Button");
+
+  constructor(object, context) {
+    super(object, context);
+    this.title = object.children[0].children[0].name;
+  }
+
+  render() {
+    return {};
   }
 }
 
-const xf = new Transformer();
-xf.when((o) => o.name.startsWith("Button"), Button)
-  .when((o) => o.type === "FRAME", Container)
-  .when((o) => o.name === "Label", Component)
-  .when((o) => o.type === "TEXT", Component)
-  .when((o) => o.name === "TextBox", Component)
-  .when((o) => o.type === "CANVAS", Container);
-
-console.log(JSON.stringify(xf.run(data), null, 2));
+console.log(JSON.stringify(transform.run(data), null, 2));
